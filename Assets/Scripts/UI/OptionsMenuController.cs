a1fafb779de303940558c869e015f6d3d42abd75
Benedikt Kirchmeier <33699887+BeneDev@users.noreply.github.com>
2018-09-13 15:45:05 +0200

    Rough Options menu added

---
+++ Assets/Scripts/UI/OptionsMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuController : MonoBehaviour {
+
+    //[SerializeField] 
+
+    private void OnEnable()
+    {
+        Invoke("SelectFirstElement", 0.3f);
+    }
+
+    void SelectFirstElement()
+    {
+
+    }
+
+    public void Back()
+    {
+
+    }
+
+}
